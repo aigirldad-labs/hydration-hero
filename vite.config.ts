f4f6c0c2c92469bf5f1d77ed48a1bc02ad08eb67
aigirldad-labs <aigirldad.labs@gmail.com>
2026-01-05 13:39:29 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/stay-hydrated/" : "/",
+  base: mode === "production" ? "/hydration-hero/" : "/",
   server: {
     host: "::",
     port: 8080,
